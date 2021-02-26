ecb5e3bced3607d127a64544c083979400d4b57d
premdhanawade <77713946+premdhanawade@users.noreply.github.com>
2021-02-26 17:23:43 +0530

    Delete Assignment1_5.py

--- Assignment1_5.py
+++
@@ -1,8 +0,0 @@
-#Write a program which display 10 to 1 on screen.
-#------------------------------------------------------------------------------------------
-def Number():
-          i=10
-          while i>0:
-                    print(i,end=" ")
-                    i-=1
-Number()
