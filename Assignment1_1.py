c137df235889aec7d52646af949bc42ddbb3fd50
premdhanawade <77713946+premdhanawade@users.noreply.github.com>
2021-02-26 17:22:28 +0530

    Delete Assignment1_1.py

--- Assignment1_1.py
+++
@@ -1,6 +0,0 @@
-#Write a program which contains one function named as Fun(). That function should display “Hello from Fun” on console.
-#------------------------------------------------------------------------------------------------------------------
-def fun():
-          print("Hello from Fun")
-
-fun()
